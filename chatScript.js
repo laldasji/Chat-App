ee1e6379a6bb86dd5a905b9f93ce300f9db2d940
Abhinav Ashutosh <abhinavashutoshlaldas@gmail.com>
2024-06-03 19:11:12 +0530

    Basic front-end functionality

---
+++ chatScript.js
@@ -0,0 +1,192 @@
+// object format ==> {channelID : channel}
+let channelMap = {};
+let activeChannelID ='';
+
+const messageDisplay = document.getElementById('messageDisplay');
+const sendButton = document.querySelector("#sendButton");
+const textContainer = document.querySelector("#textMessage");
+const userName = document.querySelector("#userName");
+const channelList = document.querySelector("#activeChannels");
+const addChannelPopup = document.querySelector("#addChannelPopup");
+const confirmAddChannel = document.querySelector("#confirmAddChannel");
+const addChannelButton = document.querySelector("#addChannel");
+const newChannelName = document.querySelector("#newChannelName");
+const newChannelID = document.querySelector("#newChannelID");
+const channelNameDisplay = document.querySelector("#channelNameDisplay");
+const addAlert = document.querySelector("#addAlert");
+
+class user
+{
+    userID = '';
+    constructor(userID)
+    {
+        this.userID = userID;
+    }
+};
+
+let userInformation = new user(sessionStorage.getItem('userID'));
+userName.textContent = userInformation.userID;
+
+class Message
+{
+    messageContent = '';
+    senderID = '';
+    constructor(messageContent, senderID)
+    {
+        this.messageContent = messageContent;
+        this.senderID = senderID;
+    }
+    addToChannel(channelID)
+    {
+        channelMap[channelID].ChannelMessages.push(this);
+    }
+    displayMessage()
+    {
+        const messageHTML = document.createElement("div");
+        messageHTML.classList.add("Message");
+
+        if (this.senderID === userInformation.userID)
+            messageHTML.classList.add("user");
+        else
+            messageHTML.classList.add("sender");
+
+        const senderDetails = document.createElement("div");
+        senderDetails.classList.add("userIdDisplay");
+        senderDetails.textContent = this.senderID;
+        messageHTML.appendChild(senderDetails);
+
+        const messageContent = document.createTextNode(this.messageContent);
+        messageHTML.appendChild(messageContent);
+
+        messageDisplay.appendChild(messageHTML);
+    }
+
+    static displayAllMessages(channelID)
+    {
+        messageDisplay.innerHTML = '';
+        let currentMessageList = channelMap[activeChannelID].ChannelMessages;
+        for (let i = 0; i < currentMessageList.length; i++)
+            currentMessageList[i].displayMessage();
+    }
+};
+
+class channel
+{
+    channelName = '';
+    channelID = '';
+    ChannelMessages = [];
+    constructor(channelName, channelID)
+    {
+        this.channelName = channelName;
+        this.channelID = channelID;
+    }
+};
+
+function generateKey(channelName, channelID)
+{
+    return channelName + '-#!' + channelID;
+}
+
+function addChannelToList()
+{
+    channelToAddName = newChannelName.value.trimRight();
+    channelToAddID = newChannelID.value.trimRight();
+
+    if (channelToAddName === '' || channelToAddID === '')
+    {
+        addAlert.style.backgroundColor = 'black';
+        addAlert.style.padding = '1%';
+        addAlert.style.color = 'white';
+        addAlert.textContent = 'Null values not allowed';
+        return;
+    }
+
+    const key = generateKey(channelToAddName, channelToAddID);
+
+    if (channelMap[key])
+    {
+        addAlert.style.backgroundColor = 'red';
+        addAlert.style.padding = '1%';
+        addAlert.style.color = 'white';
+        addAlert.textContent = 'Error! Duplicate Channel ID created';
+        return;
+    }
+
+    const newChannel = new channel(channelToAddName, channelToAddID);
+    // feed details to server
+
+    // if confirmed from server
+
+    channelMap[key] = newChannel;
+
+    const channelButton = document.createElement("div");
+    channelButton.classList.add("channelSelector");
+    channelButton.id = key;
+
+    const channelButtonText = document.createElement("div");
+    channelButtonText.textContent = channelToAddName;
+
+    const deleteChannelButton = document.createElement("button");
+    deleteChannelButton.textContent = 'Delete';
+
+    deleteChannelButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        channelNameDisplay.textContent = '';
+        delete channelMap[channelButton.id];
+        channelList.removeChild(channelButton);
+    });
+
+    channelButton.addEventListener('click', () => {
+        activeChannelID = channelButton.id;
+        channelNameDisplay.textContent = channelButtonText.textContent;
+        Message.displayAllMessages(activeChannelID);
+    });
+
+    channelButton.appendChild(channelButtonText);
+    channelButton.appendChild(deleteChannelButton);
+    channelList.insertBefore(channelButton, addChannelButton);
+
+    // remove addChannel popup from display
+    newChannelName.value = '';
+    newChannelID.value = '';
+    addChannelPopup.style.display = 'none';
+}
+
+newChannelName.addEventListener('keydown', (event) => {
+    if (event.which === 13)
+        newChannelID.focus();
+});
+
+newChannelID.addEventListener('keydown', (event) => {
+    if (event.which === 13)
+        addChannelToList();
+});
+
+confirmAddChannel.addEventListener("click", () => addChannelToList());
+
+addChannelButton.addEventListener("click", () => {
+    addChannelPopup.style.display = 'flex';
+});
+
+const sendMessage = () => {
+    if (textContainer.value != '' && activeChannelID != '')
+    {
+        const currentChannel = channelMap[activeChannelID];
+        const newInput = new Message(textContainer.value, userInformation.userID);
+        newInput.addToChannel(activeChannelID);
+        newInput.displayMessage();
+        textContainer.value = null;
+        messageDisplay.scrollTop = messageDisplay.scrollHeight;
+    }
+}
+    
+textContainer.addEventListener("keydown", (event) => {
+    if (event.which === 13) {
+        event.preventDefault();
+        sendMessage();
+    }
+});
+
+sendButton.addEventListener("click", sendMessage);
+
+textContainer.focus();
---
+++ homeScript.js
@@ -0,0 +1,53 @@
+const loginIdStatus = document.querySelector("#loginIdStatus");
+const loginButton = document.querySelector("#register");
+const usernameInput = document.querySelector("#inputUsername");
+
+const usernameRules = document.createElement("div");
+usernameRules.style.fontSize = '1.25rem';
+usernameRules.style.backgroundColor = "pink";
+usernameRules.style.padding = '1rem';
+usernameRules.textContent = "Username must contain Alphanumeric characters; no spaces, special characters allowed";
+
+const checkValidUsername = (userName) => {
+    if (userName === '' || userName.length > 32)
+        return false;
+    for (let i = 0; i < userName.length; i++)
+    {
+        if (!(((userName[i] >= 'A' && userName[i] <= 'Z') || (userName[i] >= 'a' && userName[i] <= 'z')) || (userName[i] >= '0' && userName[i] <= '9')))
+            return false;
+    }
+    console.log(userName + 'is valid');
+    return true;
+};
+
+const performLogin = (userName) => {
+    if (checkValidUsername(userName))
+    {
+        loginIdStatus.textContent = '';
+        // verify from server
+        
+        // process server's response
+        // case I: occupied
+        
+        // case II: invalid username
+
+        // case III: Valid, username granted
+        sessionStorage.setItem('userID', userName);
+        window.location.href = 'chatInterface.html';
+    }
+    else
+    {
+        loginIdStatus.appendChild(usernameRules);
+    }
+};
+
+loginButton.addEventListener('click', () => performLogin(usernameInput.value));
+
+usernameInput.addEventListener("keydown", (event) => {
+    if (event.which === 13) {
+        event.preventDefault();
+        performLogin(usernameInput.value);
+    }
+});
+
+usernameInput.focus();
